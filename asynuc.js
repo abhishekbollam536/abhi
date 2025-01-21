48ebd2a3195d73ff0050230f469b3f2da4865b1f
abhishek <abhishekbollam536@gmail.com>
2025-01-21 11:38:31 +0530

    fsd

---
+++ asynuc.js
@@ -0,0 +1,54 @@
+// console.log(10);
+// console.log(20);
+// setTimeout(()=>{
+//     console.l0g(30);
+// },5000)
+// console.log(40);
+// console.log(50);
+
+setInterval(()=>{
+    document.writeln("surya");
+},5000)
+
+
+
+
+
+// let p1=new Promise((resolve,reject)=>{});
+// console.log(p1);
+
+// let p2=new Promise((resolve,reject)=>{
+//     resolve("success");
+// });
+// console.log(p2);
+// p2.then((response)=>{
+//     console.log(response);
+// })
+// p2.catch((error)=>{
+//     console.log(error);
+// })
+
+// p2.then((response)=>{
+//     console.log(response);
+// }).catch((error)=>{
+//     console.log(error);
+// }).finally(()=>{
+//     console.log("final");
+    
+// })
+
+// let p3=new Promise((resolve,reject)=>{
+//     reject("failure");
+// });
+// // console.log(p3);
+// p3.then((response)=>{
+//     console.log(response);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+// .finally(()=>{
+//     console.log("finalhy block");
+    
+// })
+
